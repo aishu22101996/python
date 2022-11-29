155aae113d553fd953f6bbca1bada4ad9c997a7e
aishu22101996 <96903462+aishu22101996@users.noreply.github.com>
2022-11-29 12:24:41 +0530

    Update and rename Rr.py to Rr letter.py

---
+++ Rr letter.py
@@ -0,0 +1,5 @@
+let=input("enter a letter:")
+if letter=="R" or letter=="r":
+    print("letter is valid")
+else:
+    print("letter is invalid")    
--- Rr.py
+++
@@ -1,5 +0,0 @@
-let=input("enter a letter:")
-if let=="R" or let=="r":
-    print("let is valid")
-else:
-    print("let is invalid")    
