db41722de85d523a776d9a6fba19806e711f0eaa
aishu22101996 <96903462+aishu22101996@users.noreply.github.com>
2022-11-29 12:31:26 +0530

    Delete amm.py

--- amm.py
+++
@@ -1,9 +0,0 @@
-Amount=int(input("enter a amount:"))
-Amountt=Amount/1000
-tempt=Amount%1000
-Amountf=tempt/500
-tempf=tempt%500
-Amounth=tempf/100
-print("number of thousand:",Amountt)
-print("number of five hundred:",Amountf)
-print("number of hundred:",Amounth)
